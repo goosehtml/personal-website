6b9b217c588d4821344659d33e69e170908a74e5
renderGUI <90111307+renderGUI@users.noreply.github.com>
2022-05-24 15:56:09 -0400

    Add dark mode icons

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 "use strict";
 
 const darkMode = localStorage.getItem("darkMode");
-const themeButton = document.getElementById("logo");
+const themeButton = document.getElementById("theme-button");
 let isEnabled;
 
 if (darkMode === null) {
   document.body.classList.remove("darkmode");
+  themeButton.textContent = "☼";
   isEnabled = false;
 } else {
   document.body.classList.add("darkmode");
+  themeButton.textContent = "☾";
   isEnabled = true;
 }
 
@@ -18,9 +20,11 @@ const toggleTheme = () => {
   if (isEnabled) {
     localStorage.setItem("darkMode", "enabled");
     document.body.classList.add("darkmode");
+    themeButton.textContent = "☾";
   } else {
     localStorage.removeItem("darkMode");
     document.body.classList.remove("darkmode");
+    themeButton.textContent = "☼";
   }
 };
 
